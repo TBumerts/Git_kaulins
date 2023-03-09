6804fc93598de65797f46d02dbc37cd46253a40f
Bumerts <tomijs.bumerts@sk.lvt.lv>
2023-03-09 12:40:11 +0200

    Cikla izveide metode un vertibas sanemsana metode

--- src/git_kaulins/Cirks.java
+++ src/git_kaulins/Cirks.java
@@ -3,14 +3,16 @@ package git_kaulins;
 import java.util.Random;
 
 public class Cirks {
-static void mestKaulinu(){
+static void mestKaulinu(int reizes){
 	int skaitlis;
 	Random rand = new Random();
+	for(int i=1; i<=reizes; i++){
 	skaitlis = rand.nextInt(6)+1;
 	System.out.println("Uzkrita skaitlis: "+skaitlis);
 }
+}
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 
 	}
 }
