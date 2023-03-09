cbfd14457726e89acc93814ef62dd77366a2da1c
Bumerts <tomijs.bumerts@sk.lvt.lv>
2023-03-09 12:56:17 +0200

    Nodrosinata iespeja ievadit metienu x skaitu

--- src/git_kaulins/Cirks.java
+++ src/git_kaulins/Cirks.java
@@ -1,14 +1,19 @@
 package git_kaulins;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Cirks {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis,reizes;
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu?");
+		reizes = scan.nextInt();
+		for(int i=1;i<=reizes;i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
-
+		}
 	}
 }
