0ac6a5a3857bb6f1e2a29bf77a08242ce15af4ae
Bumerts <tomijs.bumerts@sk.lvt.lv>
2023-03-09 12:17:03 +0200

    Ieviesta metode kaulina mesanai

--- src/git_kaulins/Cirks.java
+++ src/git_kaulins/Cirks.java
@@ -3,12 +3,14 @@ package git_kaulins;
 import java.util.Random;
 
 public class Cirks {
-
+static void mestKaulinu(){
+	int skaitlis;
+	Random rand = new Random();
+	skaitlis = rand.nextInt(6)+1;
+	System.out.println("Uzkrita skaitlis: "+skaitlis);
+}
 	public static void main(String[] args) {
-		int skaitlis;
-		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		mestKaulinu();
 
 	}
 }
